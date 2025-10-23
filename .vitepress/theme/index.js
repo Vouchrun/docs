6480d13dae2d94a09300c7fb35ff20f2be389909
ravirasadiya <ravirasadiya1994@gmail.com>
2025-10-23 19:14:08 +0530

    Implement VouchWidget component and update layout for modal integration; refine meta tags in index.md

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -4,16 +4,20 @@ import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client';
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
+import VouchWidget from './components/VouchWidget.vue';
+import Layout from './Layout.vue';
 //import { Steps, Tab, Tabs } from 'nextra-theme-docs';
 
 import './index.css';
 
 export default {
   ...DefaultTheme,
+  Layout,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
     // Register global components, if you don't want to use it, you don't need to add it
     ctx.app.component('vImageViewer', vImageViewer);
+    ctx.app.component('VouchWidget', VouchWidget);
     // ...
   },
   setup() {
